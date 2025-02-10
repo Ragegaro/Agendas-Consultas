f232624dbfac9ef849108a3143099a07ef7cd7ee
Raul <ragegaro@gmail.com>
2025-02-10 17:10:10 -0600

    limpieza de codigo o algo asi

--- consultas-Medicas/source.cpp
+++ consultas-Medicas/source.cpp
@@ -4,12 +4,7 @@ using namespace std;
 void main() {
 	cout << "Hello repo";
 	cout << "modificamos lo existente";
-	
- 
-
-
 	cout << "Estamos en main ";
-
 	cout << "vvolvemo a probar";
 	cout << "vvolvemo y ahora creamos un merge";
 
