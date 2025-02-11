5b4382fadc6004f4ecea3f10a447579013ce13c9
Raul <ragegaro@gmail.com>
2025-02-11 11:55:46 -0600

    se crearon las primeras vent5anas de WIN API

--- consultas-Medicas/source.cpp
+++ consultas-Medicas/source.cpp
@@ -1,13 +1,36 @@
-#include <iostream>
+#include <Windows.h>
+#include"resource.h"
 
-using namespace std;
-void main() {
-	cout << "Hello repo";
-	cout << "modificamos lo existente";
-	cout << "Estamos en main ";
-	cout << "vvolvemo a probar";
-	cout << "vvolvemo y ahora creamos un merge";
+LRESULT CALLBACK fVentanaPrincipal(HWND,UINT,WPARAM,LPARAM);
 
+
+int WINAPI WinMain(HINSTANCE hInst,HINSTANCE hPrev,PSTR cmdLine,int cShow) {
 	
+	HWND hVentanaPrincipal = CreateDialog(hInst,MAKEINTRESOURCE (DLG_PRINCIPAL),NULL, fVentanaPrincipal);
+	MSG msg;
+	ZeroMemory(&msg,sizeof(MSG));
+	ShowWindow(hVentanaPrincipal,cShow);
+	while (GetMessage(&msg,NULL,NULL,NULL)){
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+		
+	}
 
+	return 0;
+}
+LRESULT CALLBACK fVentanaPrincipal(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg) {
+		case WM_CLOSE: {
+			DestroyWindow(hwnd);
+		} break;
+		case WM_DESTROY: {
+			PostQuitMessage(117); //Es ventanaPrincipal? si si, si ponlo
+		}
+		
+	}
+
+
+
+
+	return FALSE;
 }
